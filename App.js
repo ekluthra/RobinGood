099e42cd801b48e45ef6a5b8e5ce44ef2ada7631
ekluthra <ekluthra@yahoo.com>
2021-02-20 00:56:40 -0500

    landing page skeleton

--- App.js
+++ App.js
@@ -1,31 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
+import Header from './components/Header';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.textstyle}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-
-      <TouchableOpacity
-        onPress={() => alert('Hello!')}
-        style = {{backgroundColor: 'blue'}}
-      >
-        <Text style={{fontSize:20, color:'white'}}>
-          Press mi!
-        </Text>
-      </TouchableOpacity>
+      <Header title="RobinGood"/>
+      <Text style={styles.textstyle}>
+        This is the home page
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#2FA781',
+    flex:1,
+    backgroundColor: '#b8cff5',
     
   },
   textstyle: {
