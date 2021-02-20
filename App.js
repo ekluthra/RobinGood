b9f7bd4016a6fb5fa8d7cac2c0d78ad6e98db04b
ekluthra <ekluthra@yahoo.com>
2021-02-20 12:52:58 -0500

    messaging

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { LinearGradient } from 'expo-linear-gradient';
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.circle}/>
       <Header title="RobinGood"/>
       <Text style={styles.textstyle}>
         This is the home page
@@ -25,4 +26,13 @@ const styles = StyleSheet.create({
     color: 'black',
     fontSize: 18,
   },
+  circle:{
+    width: 500,
+    height:500,
+    borderRadius:250,
+    backgroundColor:'#4c8cf5',
+    position:'absolute',
+    left:100,
+    bottom:-176,
+  }
 });
