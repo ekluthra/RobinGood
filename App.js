d2472fcb66d8df38b9ebf0183efa72480a71ea04
ekluthra <ekluthra@yahoo.com>
2021-02-19 23:15:33 -0500

    button

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textstyle}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <TouchableOpacity
+        onPress={() => alert('Hello!')}
+        style = {{backgroundColor: 'blue'}}
+      >
+        <Text style={{fontSize:20, color:'white'}}>
+          Press mi!
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +23,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#2FA781',
+    
+  },
+  textstyle: {
+    color: 'black',
+    fontSize: 18,
   },
 });
